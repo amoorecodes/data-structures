91cf5ba902e819367385ed4cd65a19be988e4423
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-04-25 15:11:41 -0700

    Implemented queue size method for functional instantation.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,21 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  count = 0;
 
   // Implement the methods below
+  // First In First Out
+  // use shift unshift? 
 
   someInstance.enqueue = function(value) {
+
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
