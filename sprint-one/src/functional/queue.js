e853e675605d5b75cae447f86910abbcef5c566f
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-04-25 16:11:36 -0700

    Implemented Queue for functional instantation. Created a solution to shift indexes.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,13 +7,31 @@ var Queue = function() {
 
   // Implement the methods below
   // First In First Out
-  // use shift unshift? 
+  // enqueue will always push a value at index 0
+  // enqueue has to shift all other elements 1 index up
+  // dequeue has to delete last element in the queue
 
   someInstance.enqueue = function(value) {
-
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (count > 0) {
+    // count--;
+    // delete storage[count];
+    var temp = storage[0]
+    delete storage[0];
+    for(var key in storage) {
+      var index = Number(key) - 1;
+      storage[index] = storage[key];
+      delete storage[key];
+    }
+    count--;
+    return temp;
+    }
+    
+
   };
 
   someInstance.size = function() {
