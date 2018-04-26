eec0ccf6aa981148429547837655b631c13c3d08
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-04-25 19:20:44 -0700

    Implemented Stack class for functional-shared method. Find solution for subsequential add/remove.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+
+  var someInstance = {
+    storage: {}, 
+    count: 0
+  };
+  // var storage = {};
+  // var count = 0;
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop : function() {
+    if(this.count > 0) {
+      var temp = this.storage[this.count];
+      this.count--;
+      delete this.storage[this.count];
+      return temp;
+    }
+  },
+  size : function() {
+    return this.count;
+  }
+};
 
 
