1f1f01aa6aed95f3e2ea1cdeef32c99a51ef79aa
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-04-25 19:57:20 -0700

    Implemented a working solution for both Stack and Queue for functional shared sprint.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -5,18 +5,27 @@ var Queue = function() {
     storage: {},
     count: 0
   };
-
   _.extend(someInstance, queueMethods);
-  
-
+  return someInstance;
 };
 
 var queueMethods = {
-  enqueue: function() {
-
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
   },
-  unqueue: function() {
-
+  dequeue: function() {
+    if(this.count > 0) {
+      var temp = this.storage[0]
+      delete this.storage[0];
+      for(var key in this.storage) {
+        var index = Number(key) - 1;
+        this.storage[index] = this.storage[key];
+        delete this.storage[key];
+      }
+      this.count--;
+      return temp;
+    }
   },
   size: function() {
     return this.count;
