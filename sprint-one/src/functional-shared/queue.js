a48879523745075fe4d9a4db74f1b9e20c25b692
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-04-25 19:32:13 -0700

    Defined skeleton for queue class in functional shared sprint.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+    count: 0
+  };
+
+  _.extend(someInstance, queueMethods);
+  
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function() {
+
+  },
+  unqueue: function() {
+
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
