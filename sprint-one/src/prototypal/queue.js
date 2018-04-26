d981152fa5a694ff30fcfb4a32a9b8829e3c3c0c
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-04-25 23:24:00 -0700

    Implemented Prototypal Instantiation for both Stack and Queue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -7,14 +7,25 @@ var Queue = function() {
 };
 
 var queueMethods = {
-  enqueu: function() {
-    
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
   }, 
-  unqueue: function() {
-
+  dequeue: function() {
+    if(this.count > 0) {
+      var temp = this.storage[0];
+      delete this.storage[0];
+      for(var key in this.storage) {
+        var index = Number(key) - 1;
+        this.storage[index] = this.storage[key];
+        delete this.storage[key];
+      }
+      this.count--;
+      return temp;
+    }
   },
   size: function() {
-    return this.storage;
+    return this.count;
   }
 };
 
