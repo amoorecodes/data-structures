3a602373e9e9f041454d71e000861b9723564030
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-04-28 10:17:01 -0700

    Wrote skeleton for Binary Search Trees

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,30 @@
 var BinarySearchTree = function(value) {
+  var storage = {}
+
+  return storage;
 };
 
+binaryTreeMethods = {};
+
+binaryTreeMethods.right = function() {
+
+}
+
+binaryTreeMethods.left = function() {
+
+}
+
+binaryTreeMethods.insert = function() {
+
+}
+
+binaryTreeMethods.contains = function() {
+
+}
+
+binaryTreeMethods.depthFirstLog = function() {
+  
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
